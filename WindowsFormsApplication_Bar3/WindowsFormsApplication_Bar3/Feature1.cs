21e6d8226bc78c9c4e2ba96f85367296413da7ce
fotoflojoe <fotoflojoe@gmail.com>
2017-08-06 01:20:19 -0400

    Added Feature 1

---
+++ WindowsFormsApplication_Bar3/WindowsFormsApplication_Bar3/Feature1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication_Bar3
+{
+    class Feature1
+    {
+    }
+}
